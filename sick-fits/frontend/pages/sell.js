5594c4cf01eb71569dcb3ba4492d5f3a71a9a71c
Will Worth <will.worth@gmail.com>
2018-11-12 18:09:42 +0100

    base components and styling

---
+++ sick-fits/frontend/pages/sell.js
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+const Sell = props => (
+   <div>
+       <p>This is the sellpage</p>
+       <Link href="/">
+        <a>Home!</a>
+       </Link>
+       
+   </div>
+);
+
+export default Sell;
